0195106f3a38173f8d416f5da36cc4341016249d
karim728 <adambo1@students.towson.edu>
2020-05-12 15:39:43 -0400

    first commitadd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from  '@angular/router';
+import {HttpClientModule} from '@angular/common/http';
+import { Ng2GoogleChartsModule, GoogleChartsSettings } from 'ng2-google-charts';
+
+
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { CountriesComponent } from './components/countries/countries.component';
+import { CountriescardsComponent } from './cards/countriescards/countriescards.component';
+import { StatusboardComponent } from './cards/statusboard/statusboard.component';
+import { TablecardsComponent } from './cards/tablecards/tablecards.component';
+import { GraphcardComponent } from './cards/graphcard/graphcard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    CountriesComponent,
+    CountriescardsComponent,
+    StatusboardComponent,
+    TablecardsComponent,
+    GraphcardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    Ng2GoogleChartsModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent },
+      { path: 'countries', component: CountriesComponent },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
